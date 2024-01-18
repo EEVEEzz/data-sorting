0063344e5e75e3a46a66a1be5909a1e0e95fbd41
EEVEEzz <ivan@kotze.co.za>
2024-01-18 20:27:30 +0200

    extras

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import data from "../src/raw data/data.js";
 import $ from "jquery";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
-  const [input, setInputs] = useState(data);
-  const [output, setOutput] = useState();
+  const [output, setOutput] = useState([]);
+  const [hidden, setHidden] = useState(true);
+
+  const reff = useRef();
 
   // assignments
   const securityScanResults = data;
@@ -59,12 +61,17 @@ function App() {
 
   useEffect(() => {
     setOutput(resultArray);
-    $(`.info`).hide()
-  }, [input]);
+  }, []);
+
+  function hide() {
+    for (let i = 0; i < output.length; i++) {
+      $(`${".details" + i}`).slideUp();
+    }
+  }
 
   return (
-    <div>
-      {input ? (
+    <div className="">
+      {output && (
         <>
           <div className="stat-value text-center mb-2">Threats and Devices</div>
 
@@ -72,65 +79,69 @@ function App() {
             Data is imported from '/raw data/data.js'
           </div>
           <div className="text-xs text-center mb-5">
-            Replace the data.js file with new data
+            Replace the data.js file with new data, assign the data in the file
+            to the variable 'data' and export as default
           </div>
+
+          <button className="btn btn-accent btn-outline btn-sm" onClick={hide}>
+            Minimize All
+          </button>
 
           {output?.map((i, index) => (
             <div key={index} className="mb-10 ">
               <div className="">
-                <div
-                  onClick={() => {
-                    $(`.details${index}`).slideToggle();
-                  }}
-                  className="cursor-pointer bg-base-300 stat company hover:bg-base-200"
-                >
-                  <div className="font-bold text-error">
+                <div>
+                  <div
+                    onClick={() => {
+                      $(`${".details" + index}`).slideToggle();
+                    }}
+                    className="cursor-pointer bg-base-300 stat company hover:bg-base-200"
+                  >
                     {index + 1} - {i.Customer_name}
                   </div>
                 </div>
 
-                <div
-                  className={`stat details${index} info`}
-                >
-                  <div className="stat-title">Devices</div>
-                  <div>
-                    {i.devices.map((d, index) => (
-                      <div key={index} className="mb-5 bg-base-100 stat">
-                        <div className="text-accent font-bold">{d.Device}</div>
-                        <div>
-                          <div className="mt-1">
-                            <span className=" badge badge-lg badge-outline badge-error">
-                              {d.Affected_files.length} Affected Files
-                            </span>
-                            
+                {hidden && (
+                  <div className={`${"details" + index}`}>
+                    <div className="stat">
+                      <div className="stat-title">Devices</div>
+                      <div>
+                        {i.devices.map((d, index) => (
+                          <div key={index} className="mb-5 bg-base-100 stat">
+                            <div className="text-accent font-bold">
+                              {d.Device}
+                            </div>
+                            <div>
+                              <div className="mt-1">
+                                <span className="text-error">
+                                  {d.Affected_files.length} Affected Files
+                                </span>
+                              </div>
+                              {d.Affected_files.map((f, index) => (
+                                <li key={index}>{f}</li>
+                              ))}
+                            </div>
+                            <div>
+                              <div className="text-accent">Detected By</div>
+                              <div>{d.Detected_by}</div>
+                            </div>
+                            <div>
+                              <div className="text-accent">Last_infected</div>
+                              <div>{d.Last_infected}</div>
+                            </div>
+                            <div>
+                              <div className="text-accent">Threat</div>
+                              <div>{d.Threat}</div>
+                            </div>
                           </div>
-                          {d.Affected_files.map((f, index) => (
-                            <li key={index}>{f}</li>
-                          ))}
-                        </div>
-                        <div>
-                          <div className="text-accent">Detected By</div>
-                          <div>{d.Detected_by}</div>
-                        </div>
-                        <div>
-                          <div className="text-accent">Last_infected</div>
-                          <div>{d.Last_infected}</div>
-                        </div>
-                        <div>
-                          <div className="text-accent">Threat</div>
-                          <div>{d.Threat}</div>
-                        </div>
+                        ))}
                       </div>
-                    ))}
+                    </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
           ))}
-        </>
-      ) : (
-        <>
-          <div>Loading</div>
         </>
       )}
     </div>
